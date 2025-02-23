cf8b732a5323a3f346b5aa1c690eda89884fb409
Karlo Delalic <karlo.delalic1@gmail.com>
2025-02-22 21:54:15 -0800

    Productionize

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5001',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5001',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
