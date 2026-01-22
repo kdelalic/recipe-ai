e86fdb30d2965bb0bfee4c17e686a3ce7e8a0728
Karlo Delalic <karlo.delalic1@gmail.com>
2026-01-21 18:30:07 -0800

    skeleton

--- frontend/src/components/HistorySkeleton.jsx
+++ frontend/src/components/HistorySkeleton.jsx
@@ -3,7 +3,7 @@ import '../styles/HistorySkeleton.css';
 function HistorySkeleton() {
   return (
     <div className="history-skeleton">
-      {Array.from({ length: 5 }).map((_, index) => (
+      {Array.from({ length: 10 }).map((_, index) => (
         <div className="skeleton-item" key={index} />
       ))}
     </div>
